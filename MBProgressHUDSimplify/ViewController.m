4f3da60b5161283dad103b35317956797efad411
zaishi <zaishihuang@tagged.com>
2014-03-07 01:32:30 +0800

    add README and the category code.

--- MBProgressHUDSimplify/ViewController.m
+++ MBProgressHUDSimplify/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "UIViewController+MBProgressHUD.h"
 
 @interface ViewController ()
-
+@property (assign, nonatomic) CGFloat progress;
 @end
 
 @implementation ViewController
@@ -17,13 +18,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.HUD.yOffset = -50;
+    //self.HUD.minSize = CGSizeMake(120, 0);
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)showMessage:(id)sender {
+    [self showHUDMessage:@"some message"];
+}
+
+- (IBAction)showIndicator:(id)sender {
+    [self showHUDIndicatorMessage:nil duration:1];
+}
+
+- (IBAction)showIndicatorMessage:(id)sender {
+    [self showHUDIndicatorMessage:@"loading" duration:1];
+}
+- (IBAction)showSucced:(id)sender {
+    [self showHUDSuccessMessage:nil];
+}
+- (IBAction)showSuccedMessage:(id)sender {
+    [self showHUDSuccessMessage:@"complete"];
+}
+- (IBAction)showError:(id)sender {
+    [self showHUDErrorMessage:@"Error"];
+}
+- (IBAction)showProgress:(id)sender {
+    _progress = 0;
+    [self showProgress];
+}
+
+- (void)showProgress
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    _progress += 0.1;
+    [self showHUDProgress:_progress];
+    
+    if (_progress < 1) {
+        [self performSelector:@selector(showProgress) withObject:nil afterDelay:0.1];
+    }
 }
 
 @end
